f479411929ef5068d11edbd36f1255668c60d618
hd2g <nagisa.159s.8673.trap@gmail.com>
2025-05-14 02:37:42 +0900

    Update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,26 +1,28 @@
-local map = vim.keymap
+local map = vim.keymap.set
 
 -- Window Splittings
-map.set('n', '-', '<cmd>sp<cr>')
-map.set('n', '|', '<cmd>vs<cr>')
+map('n', '-', '<cmd>sp<cr>')
+map('n', '|', '<cmd>vs<cr>')
 
 -- Window Movings
-map.set('n', '<C-h>', '<C-w>h')
-map.set('n', '<C-j>', '<C-w>j')
-map.set('n', '<C-k>', '<C-w>k')
-map.set('n', '<C-l>', '<C-w>l')
-
-map.set('n', '<leader>o', '<cmd>only<cr>')
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
 
 -- Window Resizings
 -- TODO: Configure this
-map.set('n', '<C-w><C-h>', '<cmd>vertical resize -5<cr>')
-map.set('n', '<C-w><C-j>', '<cmd>resize +5<cr>')
-map.set('n', '<C-w><C-k>', '<cmd>resize -5<cr>')
-map.set('n', '<C-w><C-l>', '<cmd>vertical resize +5<cr>')
+map('n', '<C-w><C-h>', '<cmd>vertical resize -4<cr>')
+map('n', '<C-w><C-j>', '<cmd>resize +4<cr>')
+map('n', '<C-w><C-k>', '<cmd>resize -4<cr>')
+map('n', '<C-w><C-l>', '<cmd>vertical resize +4<cr>')
 
-map.set('i', '<C-f>', '<Right>')
-map.set('i', '<C-b>', '<Left>')
+map('i', '<C-f>', '<Right>')
+map('i', '<C-b>', '<Left>')
 
-map.set('n', '<S-w>', '<cmd>w<cr>')
-map.set('n', '<S-q>', '<cmd>q<cr>')
+map('n', '<leader>o', '<cmd>only<cr>')
+map('n', '<S-w>', '<cmd>write<cr>')
+map('n', '<S-q>', '<cmd>quit<cr>')
+
+map('n', '(', '<cmd>bp<cr>')
+map('n', ')', '<cmd>bn<cr>')
