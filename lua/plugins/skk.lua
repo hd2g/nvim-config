0a4b27dff80eb9bb1d17e2304d6db2002fcb30bf
hd2g <nagisa.159s.8673.trap@gmail.com>
2025-05-15 04:34:57 +0900

    Add jisyo for skk

--- lua/plugins/skk.lua
+++ lua/plugins/skk.lua
@@ -1,6 +1,9 @@
 return {
   "vim-skk/eskk.vim",
   config = function()
+    vim.cmd [[
+      let g:eskk#dictionary = { 'path': '~/.skk-jisyo', 'sorted': 0, 'encoding': 'euc-jp' }
+    ]]
     vim.keymap.set('i', 'jk', '<esc><cmd>call eskk#toggle()<cr>i')
   end
 }
