0928bad68e8d9177bbe4b87f6ae483df1f493b60
hd2g <nagisa.159s.8673.trap@gmail.com>
2025-05-15 01:53:35 +0900

    Add howm for the time being

--- init.lua
+++ init.lua
@@ -2,5 +2,6 @@ require("config.lazy")
 require("config.autocmds")
 require("config.options")
 require("config.keymaps")
+require("config.howm")
 require("config.neovide")
 
---
+++ lua/config/howm.lua
@@ -0,0 +1,12 @@
+vim.cmd [[
+set runtimepath+=~/src/github.com/fuenor/qfixhowm
+
+" キーマップリーダー
+let QFixHowm_Key = 'g'
+
+" howm_dirはファイルを保存したいディレクトリを設定
+let howm_dir             = '~/wiki'
+let howm_filename        = '%Y/%m/%Y-%m-%d-%H%M%S.txt'
+let howm_fileencoding    = 'utf-8'
+let howm_fileformat      = 'unix'
+]]
