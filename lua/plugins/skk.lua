ac7e6abab2d6bca2da72fcab6bcf5370783d5f1d
hd2g <nagisa.159s.8673.trap@gmail.com>
2025-05-19 20:32:14 +0900

    Add large dictionary

--- lua/plugins/skk.lua
+++ lua/plugins/skk.lua
@@ -3,6 +3,7 @@ return {
   config = function()
     vim.cmd [[
       let g:eskk#dictionary = { 'path': '~/.skk-jisyo', 'sorted': 0, 'encoding': 'euc-jp' }
+      let g:eskk#large_dictionary = { 'path': '/nix/store/j6pswq2z4d6hizaj00xki76dznl62cl3-skk-jisyo-l-0-unstable-2024-08-28/share/skk/SKK-JISYO.L', 'sorted': 0, 'encoding': 'euc-jp' }
     ]]
     vim.keymap.set('i', 'jk', '<esc><cmd>call eskk#toggle()<cr>i')
   end
